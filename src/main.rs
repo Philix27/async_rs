1ea79ef3a5f090702243164b42d2c072c220febc
Philix27 <philixbob@gmail.com>
2024-03-19 04:10:39 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+
+pub fn main() {
+    add(23, 12);
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
